ce8fa312142065c674324a753794edd65f3ef58a
Scott Ivey <scott.ivey@gmail.com>
2016-08-27 18:36:02 -0400

    organize

--- include/aliens/reactor/EpollReactor.h
+++ include/aliens/reactor/EpollReactor.h
@@ -1,6 +1,48 @@
 #pragma once
+
+#include <vector>
+#include <chrono>
+#include <memory>
 #include <sys/epoll.h>
+#include "aliens/reactor/EpollFd.h"
+
 namespace aliens { namespace reactor {
+
+class EpollReactor {
+ public:
+  static const size_t kMaxEvents = 1024;
+  class Task {
+   protected:
+    EpollReactor *reactor_ {nullptr};
+    void setReactor(EpollReactor *reactor);
+   public:
+    friend class EpollReactor;
+    EpollReactor* getReactor() const;
+    virtual int getFd() = 0;
+    virtual void onError() = 0;
+    virtual void onEvent() = 0;
+  };
+ protected:
+  bool running_ {false};
+  std::vector<Task*> tasks_;
+  epoll_event events_[kMaxEvents];
+  EpollFd epollFd_;
+  EpollReactor(EpollFd &&fd);
+  static EpollReactor* createPtr();
+ public:
+  static EpollReactor create();
+  static std::unique_ptr<EpollReactor> createUnique();
+  void addTask(Task *task);
+  int runOnce();
+  void loopForever();
+
+  using duration_type = std::chrono::milliseconds;
+
+  void runForDuration(duration_type minDuration);
+  void stop();
+  bool isRunning() const;
+};
+
 
 
 
---
+++ src/reactor/EpollReactor.cpp
@@ -0,0 +1,103 @@
+#include "aliens/reactor/EpollReactor.h"
+#include "aliens/reactor/EpollFd.h"
+#include "aliens/exceptions/macros.h"
+#include <memory>
+#include <glog/logging.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+
+
+namespace aliens { namespace reactor {
+
+bool epollEventHasError(const epoll_event *evt) {
+  return evt->events & EPOLLERR
+    || evt->events & EPOLLHUP
+    || (!evt->events & EPOLLIN);
+}
+
+bool epollEventHasError(const epoll_event &evt) {
+  return epollEventHasError(&evt);
+}
+
+EpollReactor* EpollReactor::Task::getReactor() const {
+  return reactor_;
+}
+void EpollReactor::Task::setReactor(EpollReactor *reactor) {
+  reactor_ = reactor;
+}
+
+EpollReactor::EpollReactor(EpollFd &&fd)
+  : epollFd_(std::forward<EpollFd>(fd)) {
+  memset((void*) &events_, '\0', sizeof(events_));
+}
+
+EpollReactor* EpollReactor::createPtr() {
+  return new EpollReactor(EpollFd::create());
+}
+
+EpollReactor EpollReactor::create() {
+  return EpollReactor(EpollFd::create());
+}
+
+std::unique_ptr<EpollReactor> EpollReactor::createUnique() {
+  return std::unique_ptr<EpollReactor>(createPtr());
+}
+
+void EpollReactor::addTask(Task *task) {
+  epoll_event evt;
+  evt.data.ptr = (void*) task;
+  evt.events = EPOLLIN | EPOLLET;
+  ALIENS_CHECK_SYSCALL(epoll_ctl(
+    epollFd_.get(), EPOLL_CTL_ADD, task->getFd(), &evt
+  ));
+  task->setReactor(this);
+  tasks_.push_back(task);
+}
+
+int EpollReactor::runOnce() {
+  int nEvents = epoll_wait(epollFd_.get(), events_, kMaxEvents, 20);
+  for (size_t i = 0; i < nEvents; i++) {
+    auto task = (Task*) events_[i].data.ptr;
+    if (epollEventHasError(events_[i])) {
+      task->onError();
+      continue;
+    } else {
+      task->onEvent();
+    }
+  }
+  return nEvents;
+}
+
+void EpollReactor::loopForever() {
+  running_ = true;
+  for (;;) {
+    runOnce();
+    if (!running_) {
+      break;
+    }
+  }
+}
+
+using duration_type = typename EpollReactor::duration_type;
+void EpollReactor::runForDuration(duration_type minDuration) {
+  auto start = std::chrono::system_clock::now().time_since_epoch();
+  for (;;) {
+    runOnce();
+    auto now = std::chrono::system_clock::now().time_since_epoch();
+    auto elapsed = now - start;
+    if (elapsed >= minDuration) {
+      break;
+    }
+  }
+}
+
+void EpollReactor::stop() {
+  CHECK(running_);
+  running_ = false;
+}
+
+bool EpollReactor::isRunning() const {
+  return running_;
+}
+
+}} // aliens::reactor
